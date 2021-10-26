9a65670c7355ec3b13afe3d6f8f41fedc68370cc
NickCherepashkin <cherepawka13@mail.ru>
2021-10-26 03:43:54 +0300

    add play_one_track_fragment

--- app/src/main/java/com/drozdova/musicplayerapp/MainActivity.kt
+++ app/src/main/java/com/drozdova/musicplayerapp/MainActivity.kt
@@ -2,10 +2,27 @@ package com.drozdova.musicplayerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.FragmentManager
+import com.drozdova.musicplayerapp.databinding.ActivityMainBinding
+import com.drozdova.musicplayerapp.view.FragmentPlayOneTrack
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        startFragment()
+    }
+
+    private fun startFragment() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, FragmentPlayOneTrack())
+            .commit()
     }
 }
